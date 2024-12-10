cdfe37f6b6b0399f53000eedaf1c6fad397858ee
Azha <azhafauz13@gmail.com>
2024-12-10 23:07:37 +0530

    Add TrustedBy component and update Featured component

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,46 +1,56 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Featured.scss';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Featured.scss";
+import { TrustedBy } from "../../components";
 
 const Featured = () => {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const navigate = useNavigate();
-  
+
   const handleSearch = () => {
-    if(search) {
+    if (search) {
       navigate(`/gigs?search=${search}`);
     }
-  }
+  };
 
   return (
-    <div className='featured'>
+    <div className="featured">
       <div className="container">
-
-        <div className="left">
-          <h1>Find the perfect <span>freelance</span> services for your business</h1>
+        <div className="Boy1">
+          <img src="./media/Boy1.png" alt="boy1" />
+        </div>
+        <div className="Girl1">
+          <img src="./media/Girl1.png" alt="girl1" />
+        </div>
+        <div className="center">
+          <h1>
+            Scale your professional workforce with <span>freelance</span>
+          </h1>
           <div className="search">
             <div className="searchInput">
-              <img src="./media/search.png" alt="search" />
-              <input type="search" placeholder='Try "website"' onChange={(({ target: { value } }) => setSearch(value))} />
+              <input
+                type="search"
+                placeholder="    Search for any service..."
+                onChange={({ target: { value } }) => setSearch(value)}
+              />
+
+              <button onClick={handleSearch}>
+                <img src="./media/search.png" alt="search" />
+              </button>
             </div>
-            <button onClick={handleSearch}>Search</button>
           </div>
-          <div className="popular">
-            <span>Popular:</span>
-            <button>Website Design</button>
-            <button>WordPress</button>
-            <button>Logo Design</button>
-            <button>AI Services</button>
-          </div>
+          <TrustedBy />
         </div>
 
-        <div className="right">
-          <img src="./media/hero.png" alt="hero" />
+        <div className="Girl2">
+          <img src="./media/Girl2.png" alt="girl2" />
         </div>
-        
+        <div className="Boy2">
+          <img src="./media/Boy2.png" alt="boy2" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
